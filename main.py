ac6edee23aac4bbf0e0f13bec615a4c6287c02e8
Mohdbilal123 <92420280+Mohdbilal123@users.noreply.github.com>
2021-10-30 10:09:25 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ from PIL import ImageTk, Image
 
 class All_FUNCTION:
     def about(self):
-       print("rajat")
+       f=Frame(root,width=500,height=500).place(x=0,y=0)
+       Button(f,text='Back',command=self.home).place(x=20,y=20)
+       
     def ITEMS(self):
         #msg.showinfo('INFO','This is RAJAT\'s BAKERY')
 
@@ -28,27 +30,19 @@ class Main(All_FUNCTION):
         self.root.config(bg=self.yellow)
         self.root.title('rajat')
         #database initialise
-        self.query=KEEpydb.query('bakeryshop','rajat','rajat@143')
+        self.query=KEEpydb.query('sriganeshbakery','rajat123',324146566)
         
         #load items
-        self.img=ImageTk.PhotoImage(Image.open('bakeryimg\\rajat.jpg').resize((800,700), Image.ANTIALIAS))
-        self.items={
-            "white forest1 kg": [10,400],
-            "black forest1 kg": [10,350],
+        self.img=ImageTk.PhotoImage(Image.open('assets\\rajat.jpg').resize((800,700), Image.ANTIALIAS))
+        self.items={}
 
-            "pinp cake250 gm":  [10,250],
-            "butter scotch250 gm  ":[10,250],
-            "chocolate250 gm ": [10,200],
-            "antique4 lts":  [10,862],
-            "antique1 lts":    [20,223],
-            "shyne20 lts":      [10,2411],
-            "shyne10 lts":      [10,1273],
-            "shyne4 lts":     [10,546],
-            "shyne1 lts":      [25,144],
-            "classic20 lts":    [15,1857],
-            "classic10 lts":    [10,979],
-            "classic4 lts":    [12,424],
-            "classic1 lts":    [14,114]}
+        c=2
+        for i in range(6):
+            self.items[self.query.get_cell('a'+str(c))]=[self.query.get_cell('b'+str(c)),self.query.get_cell('c'+str(c)),self.query.get_cell('d'+str(c))]
+            c+=1                      
+            
+        
+        
 
         self.home()
         
@@ -60,14 +54,14 @@ class Main(All_FUNCTION):
         a.place(x=-100,y=-100)
         Label(frame,text='SRI GANESH BAKERY  !',font='verdana 20 bold',bg=self.yellow).place(x=20,y=70)
         Button(frame,text='  ABOUT',bg='white',padx=50,command=self.about).place(x=50,y=150)
-        Button(frame,text='BUY',bg='white',padx=60,command=self.showitems).place(x=50,y=190)
-        Button(frame,text='ITEMS',bg='white',padx=54,command=self.showitems).place(x=50,y=230)
+        Button(frame,text='BUY',bg='white',padx=60,command=self.buyitems).place(x=50,y=190)
+        Button(frame,text='ITEMS',bg='white',padx=54,command=self.buyitems).place(x=50,y=230)
         Button(frame,text='PAYMENT',bg='white',padx=45).place(x=50,y=270)
         Button(frame,text='CUSTOMER DETAILS',bg='white',padx=20).place(x=50,y=310)
         Button(frame,text='EXIT',bg='white',padx=57).place(x=50,y=350)
         
-    def showitems(self):
-        self.img=ImageTk.PhotoImage(Image.open('bakeryimg\\i.jpg').resize((800,700), Image.ANTIALIAS))
+    def buyitems(self):
+        self.img=ImageTk.PhotoImage(Image.open('assets\\d.jpg').resize((800,700), Image.ANTIALIAS))
         frame=Frame(self.root,width=self.width,
                     height=self.height,bg='white').place(x=0,y=0)
         l=Listbox(frame)
@@ -75,10 +69,19 @@ class Main(All_FUNCTION):
         l.place(x=20,y=60,width=200,height=400)
         for i in self.items.keys():
             l.insert(END,i)
-
-        Button(frame,text='Add TO Cart',bg='yellow').place(x=300,y=50,width=100)
-        Button(frame,text='Next',bg='yellow',command=self.bookings).place(x=300,y=80,width=100)
+        global quantity
+        self.q=0
+        quantity=Label(frame,text='Quantity : '+str(self.q),font='verdana 13 bold')
+        quantity.place(x=300,y=400)
+        
     
+
+        Button(frame,text='Add TO Cart',bg='yellow',command=self.inc).place(x=300,y=50,width=100)
+        Button(frame,text='Next',bg='yellow',command=self.bookings).place(x=300,y=80,width=100)
+    def inc(self):
+        global quantity
+        self.q+=1
+        quantity.config(text='Quantity : '+str(self.q))
     def bookings(self):
         frame=Frame(self.root,width=self.width,
                     height=self.height,bg='white').place(x=0,y=0)
